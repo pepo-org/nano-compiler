1be3d3e1dbc15ee5161560f765f4e782b2714f19
Matthew Majfud-Wilinski <matthewmajf@gmail.com>
2022-06-09 09:54:57 -0700

    Made the compiler identify comments and identify arguments.

--- nano-compiler.py
+++ nano-compiler.py
@@ -8,6 +8,8 @@ parser.add_argument('--compile', type=str, required=True)
 parser.add_argument('--output', type=str, required=True)
 args = parser.parse_args()
 
+DEBUG = True
+
 number_of_lines_counted = 0
 indent = 0
 
@@ -16,5 +18,19 @@ with open(args.output, 'w') as f:
         for line in topo_file:
             number_of_lines_counted = number_of_lines_counted + 1
             if line.startswith('$'):
-                asm_file_contents
-            
+                if DEBUG == True:
+                    f.write(';' + line[1:])
+                    print('Found a comment! This comment is ' + str(len(line[1:])) + ' characters long...')
+                elif DEBUG == False:
+                    f.write(';' + line[1:])
+            elif line.startswith('function '):
+                if DEBUG == True:
+                    function_name = str(line[9:]).split()[0]
+                    function_name_with_args = line[9:]
+                    args = function_name_with_args[len(function_name) + 1:].split(', ')
+                    print('A function was declared with the name of `' + function_name + '`...')
+                    print('The function comes with ' + str(len(args)) + ' arguments, and those arguments are `' + str(args) + '`...')
+                elif DEBUG == False:
+                    function_name = str(line[9:]).split()[0]
+                    function_name_with_args = line[9:]
+                    args = function_name_with_args[len(function_name) + 1:].split(', ')
